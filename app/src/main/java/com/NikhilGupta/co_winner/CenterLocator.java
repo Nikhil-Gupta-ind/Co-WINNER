62acbda405172cde871f8b8b80ec655778c6f66d
Nikhil Gupta <85886260+jcodeNikhil@users.noreply.github.com>
2021-09-24 11:57:12 +0530

    Center Locator Activity

---
+++ app/src/main/java/com/NikhilGupta/co_winner/CenterLocator.java
@@ -0,0 +1,15 @@
+package com.NikhilGupta.co_winner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CenterLocator extends AppCompatActivity {
+
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_center_locator);
+    }
+}
--- app/src/main/java/com/NikhilGupta/co_winner/MainActivity.java
+++ app/src/main/java/com/NikhilGupta/co_winner/MainActivity.java
@@ -2,13 +2,33 @@ package com.NikhilGupta.co_winner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.NikhilGupta.co_winner.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    public void onBackPressed() {
+        finishAffinity();
+    }
+
+    ActivityMainBinding binding;
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.textViewLoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CenterLocator.class));
+            }
+        });
     }
 }
