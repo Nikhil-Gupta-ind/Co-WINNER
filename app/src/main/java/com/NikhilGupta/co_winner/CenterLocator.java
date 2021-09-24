a5bac2e0c799f99c7dd737960985688072a0fdca
Nikhil Gupta <85886260+jcodeNikhil@users.noreply.github.com>
2021-09-24 12:20:34 +0530

    waiting date picker

--- app/src/main/java/com/NikhilGupta/co_winner/CenterLocator.java
+++ app/src/main/java/com/NikhilGupta/co_winner/CenterLocator.java
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.NikhilGupta.co_winner.databinding.ActivityMainBinding;
+
 public class CenterLocator extends AppCompatActivity {
 
-    
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_center_locator);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
     }
 }
