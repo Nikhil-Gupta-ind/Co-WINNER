87834301516d76d6dcb2b4261e4080f045ca36a7
Nikhil Gupta <85886260+jcodeNikhil@users.noreply.github.com>
2021-10-08 19:25:47 +0530

    Dashboard Prototype

--- app/src/main/java/com/NikhilGupta/co_winner/MainActivity.java
+++ app/src/main/java/com/NikhilGupta/co_winner/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Toast;
 
 import com.NikhilGupta.co_winner.databinding.ActivityMainBinding;
 
@@ -16,19 +19,48 @@ public class MainActivity extends AppCompatActivity {
     }
 
     ActivityMainBinding binding;
+    Animation animation, animation2 , animation3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.textViewLoc.setOnClickListener(new View.OnClickListener() {
+        animation = AnimationUtils.loadAnimation(this, R.anim.atg);
+        animation2 = AnimationUtils.loadAnimation(this, R.anim.atg_two);
+        animation3 = AnimationUtils.loadAnimation(this, R.anim.atg_three);
+        //Pass Animation
+        binding.imageView.setAnimation(animation);
+        binding.title.setAnimation(animation2);
+        binding.subTitle.setAnimation(animation2);
+
+        binding.centerLocator.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, CenterLocator.class));
             }
         });
+
+        binding.certificate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Working on it!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        binding.more.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Coming Soon!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        binding.more2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Coming Soon!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
